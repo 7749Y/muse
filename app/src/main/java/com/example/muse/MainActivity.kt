7a641a6a3619f9c19c16cc3b9dff207acbb713a5
nanayonkyuu <547519244@qq.com>
2026-05-03 17:05:10 +0800

    径向菜单测试

--- app/src/main/java/com/example/muse/MainActivity.kt
+++ app/src/main/java/com/example/muse/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import com.example.muse.ui.screen.browse.BrowseScreen
 import com.example.muse.ui.screen.edit.EditScreen
 import com.example.muse.ui.screen.edit.MaximizedEditScreen
+import com.example.muse.ui.screen.edit.component.RadialMenuTestScreen
 import com.example.muse.ui.screen.home.HomeScreen
 import com.example.muse.ui.theme.MuseTheme
 
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MuseTheme(darkTheme = true) {
-                EditScreen()
+                RadialMenuTestScreen()
             }
         }
     }
